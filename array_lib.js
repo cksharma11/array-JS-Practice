4a188a890360effaceebc3142e26080c39bc1b6a
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-14 13:23:30 +0530

    Added array_lib and test_array_lib
    array_lib contains all the function and test lib contains all the
    tests

---
+++ array_lib.js
@@ -0,0 +1,33 @@
+const generateFibSeries = function(nthTerm){
+  let firstTerm = -1;
+  let secondTerm = 1;
+  let fibSeries = [];
+  for(let index=0; index < nthTerm; index++){
+    let nextTerm = firstTerm + secondTerm;
+    firstTerm = secondTerm;
+    secondTerm = nextTerm;
+    fibSeries[index] = nextTerm;
+  }
+  return fibSeries;
+}
+
+const reverseSeries = function(series){
+  let reversedSeries = [];
+  for(let index = series.length-1; index >= 0; index--){
+    reversedSeries[series.length-1 - index] = series[index];
+  }
+  return reversedSeries;
+}
+
+const splitEvenOdd = function(list){
+  let evenOddList = [[],[]];
+  for(let index = 0;index < list.length; index++){
+    evenOddList[Math.abs(list[index]%2)].push(list[index]);
+  }
+  return evenOddList;  
+}
+
+
+exports.generateFibSeries = generateFibSeries;
+exports.reverseSeries = reverseSeries;
+exports.splitEvenOdd = splitEvenOdd;
---
+++ test_array_lib.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+const lib = require("./array_lib.js");
+
+const generateFibSeries = lib.generateFibSeries;
+const reverseSeries = lib.reverseSeries;
+const splitEvenOdd = lib.splitEvenOdd;
+
+/*---------- Test for generateFibSeries ------------*/
+
+//for regular numbers
+assert.deepEqual(generateFibSeries(5), [0,1,1,2,3]);
+assert.deepEqual(generateFibSeries(0), []);
+//for 1 and 2
+assert.deepEqual(generateFibSeries(1), [0]);
+assert.deepEqual(generateFibSeries(2), [0,1]);
+
+/*---------- Test for reverseSeries ----------------*/
+
+//for empty values and 1 value
+assert.deepEqual(reverseSeries([]), []);
+assert.deepEqual(reverseSeries([0]), [0]);
+//for positive and negitive numbers
+assert.deepEqual(reverseSeries([1,2]), [2,1]);
+assert.deepEqual(reverseSeries([-1,-5,5,32]), [32,5,-5,-1]);
