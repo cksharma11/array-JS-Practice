47429ec698eb919f59c552e2ec0e8c442ce28ec8
Chandan Kumar <cksharma122@gmail.com>
2018-10-17 16:31:04 +0530

    Delete testSelectEvenOdd.js

--- testSelectEvenOdd.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-const lib = require("./selectEvenOdd.js").splitEvenOdd;
-console.log(lib([1,2,5,4,3,2,1]));
-assert.deepEqual(lib([1,2,3,4,5]), [ [2,4],[1,3,5]]);
-assert.deepEqual(lib([1]), [ [],[1]]);
-assert.deepEqual(lib([1,-1,-2,5]), [ [-2],[1,-1,5]]);
-console.log(lib([1,-1,-2,5]));
